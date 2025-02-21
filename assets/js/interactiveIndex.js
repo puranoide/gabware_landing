f155af3fc4afcacd0a838e86ff6ef18b7c37ddc5
puranoide <acostasanchezangelgabriel@gmail.com>
2025-02-20 19:13:30 -0500

    llamadas de accion

---
+++ assets/js/interactiveIndex.js
@@ -0,0 +1,6 @@
+var primeraLllamada = document.getElementById("elemento-flotante-primera-llamada");
+
+
+primeraLllamada.addEventListener("click", function () { 
+    alert("Estamos trabajando en ello");
+});   
