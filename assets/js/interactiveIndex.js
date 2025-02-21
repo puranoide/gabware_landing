c440e086d53fd2f40d7f0078637d35fbb9c42238
puranoide <acostasanchezangelgabriel@gmail.com>
2025-02-20 19:28:15 -0500

    botones a contacto

--- assets/js/interactiveIndex.js
+++ assets/js/interactiveIndex.js
@@ -1,6 +1,9 @@
 var primeraLllamada = document.getElementById("elemento-flotante-primera-llamada");
-
+var contactanosbutton = document.getElementById("contactanosbutton");
 
 primeraLllamada.addEventListener("click", function () { 
-    alert("Estamos trabajando en ello");
+    window.location.href = "#contactanos";
 });   
+contactanosbutton.addEventListener("click", function () { 
+    window.location.href = "#contactanos";
+});
