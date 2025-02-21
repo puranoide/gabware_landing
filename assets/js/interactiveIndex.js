c1d9d9e219666657795f961af4456c0e1e84bfd8
puranoide <acostasanchezangelgabriel@gmail.com>
2025-02-20 20:02:35 -0500

    smoth redirect

--- assets/js/interactiveIndex.js
+++ assets/js/interactiveIndex.js
@@ -2,8 +2,8 @@ var primeraLllamada = document.getElementById("elemento-flotante-primera-llamada
 var contactanosbutton = document.getElementById("contactanosbutton");
 
 primeraLllamada.addEventListener("click", function () { 
-    window.location.href = "#contactanos";
+    window.scrollTo({ top: document.getElementById("nuestros-proyectos").offsetTop, behavior: "smooth" });
 });   
 contactanosbutton.addEventListener("click", function () { 
-    window.location.href = "#contactanos";
+    window.scrollTo({ top: document.getElementById("contactanos").offsetTop, behavior: "smooth" });
 });
