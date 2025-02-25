b0f084e82a9f1faa4d4aeb6f397751eab81c3f52
puranoide <acostasanchezangelgabriel@gmail.com>
2025-02-24 22:17:16 -0500

    creacion de modulos para blog y admin

--- assets/js/interactiveIndex.js
+++ assets/js/interactiveIndex.js
@@ -1,9 +1,13 @@
 var primeraLllamada = document.getElementById("elemento-flotante-primera-llamada");
 var contactanosbutton = document.getElementById("contactanosbutton");
-
+var interactive_touch = document.getElementById("interactive-touch");
 primeraLllamada.addEventListener("click", function () { 
     window.scrollTo({ top: document.getElementById("nuestros-proyectos").offsetTop, behavior: "smooth" });
 });   
 contactanosbutton.addEventListener("click", function () { 
     window.scrollTo({ top: document.getElementById("contactanos").offsetTop, behavior: "smooth" });
+});
+
+interactive_touch.addEventListener("click", function () { 
+   window.location.href = "./blog";
 });
